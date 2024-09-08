4378d4d9e3cf79a5386ead60b83a768ad259a200
Ruam Marcos <ruam.marcos@mail.uft.edu.br>
2024-09-08 16:39:52 -0300

    versão 3.0

--- main.py
+++ main.py
@@ -1,54 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 def linha():
-    print("\n")
-    print("-"*10)
+    print("-"*20)
 
 #Definição da função f(x)
 def f(x):
-    return x**2 - 4
-
+    return x**2 - 4 
+funcao = 'f(x) = x^2 - 4'
 
 #Recebendo os valores do intervalo [a, b]
+print("\n\n  Recebendo o intervalo [a,b]\n")
 a = float(input("a: ")) 
 b = float(input("b: ")) 
-x = float()
+xn = float()
+
+
 
 #Declaração do critério de parada
-epsilon = b - a
+epsilon = abs(b - a)
 linha()
 
+
+
+
 #Demarcando intervalo da tolerância
+print("  Tolerância ε")
 n = float(input("\nε > 10^-n   (n = 0, 1, 2...)\n\nn: "))
 linha()
 stop = 10**(-1*n)
  
 
 if(f(a) * f(b) < 0):
-
+    msg = str()
     while epsilon > stop:
 
+        if(xn != None): epsilon = xn
         
+        xn = (b + a)/2
 
-        if(x != None): epsilon = x
-        
-        x = (b + a)/2
+        epsilon = abs(xn - epsilon) 
 
-        epsilon = abs(x - epsilon) 
-
-        if(f(x) == 0):
-            print(f"x = {x} é a raiz exata")
-            print(f"f({x}) = {f(x)}")
+        if(f(xn) == 0):
+            msg = f"A raiz exata é x = {xn}"
             break
 
-        elif f(a) * f(x) < 0:
-            b = x
+        elif f(a) * f(xn) < 0:
+            b = xn
         
         else:
-            a = x
+            a = xn
+        msg = f"A raiz aproximada é x1 = {xn}"
 
-        #epsilon = abs(f(x))
-
-        print(f"f({x}) = {f(x)}")
+    print(msg)
 
 else:
     print("Não há raiz no intervalo pois: ")
@@ -56,3 +61,31 @@ else:
     print(f"f(a)*f(b) >= 0")
 
 linha()
+print("\n\n")
+
+
+
+
+
+
+#Plotando o gráfico
+
+# Crie um array de valores x
+x = np.linspace(a-5, b+5, 400)
+
+#Criando um array dos valores de y
+y = f(x)
+
+#Setando a funcao
+plt.plot(x, y, label=funcao)
+
+#Configurando algumas coisas no gráfico
+plt.title('Gráfico de f(x)')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.axhline(0, color='red',linewidth=0.5)  # Linha horizontal no eixo y=0
+plt.axvline(0, color='red',linewidth=0.5)  # Linha vertical no eixo x=0
+plt.legend()
+plt.grid(True)
+ 
+plt.savefig('grafico.png', dpi=300, bbox_inches='tight')
