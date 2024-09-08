b97e7ccb8239250fe8209a9d463dd50eb1e23dd4
Ruam Marcos <ruam.marcos@mail.uft.edu.br>
2024-09-08 17:22:46 -0300

    versão 4.0

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 def linha():
     print("-"*20)
 
+
 #Definição da função f(x)
 def f(x):
-    return x**2 - 4 
-funcao = 'f(x) = x^2 - 4'
+    return x**3 - 3 
+funcao = 'f(x) = x^3 - 3'
+
 
 #Recebendo os valores do intervalo [a, b]
 print("\n\n  Recebendo o intervalo [a,b]\n")
@@ -17,12 +20,9 @@ b = float(input("b: "))
 xn = float()
 
 
-
 #Declaração do critério de parada
 epsilon = abs(b - a)
 linha()
-
-
 
 
 #Demarcando intervalo da tolerância
@@ -32,6 +32,25 @@ linha()
 stop = 10**(-1*n)
  
 
+#Criando os dados da tabela
+dataTabela = {
+    "n":list(),
+    "an":list(),
+    "bn":list(),
+    "xn":list(),
+    "f(xn)":list(),
+    "ε":list()
+}
+
+n = 0
+dataTabela['n'].append(int(n))
+dataTabela["an"].append(round(a, 5))
+dataTabela["bn"].append(round(b, 5))
+dataTabela["ε"].append("--")
+dataTabela["xn"].append(xn)
+dataTabela["f(xn)"].append(f(xn))
+
+#Calculando o valor aproximado de x1
 if(f(a) * f(b) < 0):
     msg = str()
     while epsilon > stop:
@@ -39,7 +58,8 @@ if(f(a) * f(b) < 0):
         if(xn != None): epsilon = xn
         
         xn = (b + a)/2
-
+        
+        
         epsilon = abs(xn - epsilon) 
 
         if(f(xn) == 0):
@@ -51,10 +71,18 @@ if(f(a) * f(b) < 0):
         
         else:
             a = xn
+
+        if(n > 0):
+            dataTabela["n"].append(int(n))
+            dataTabela["an"].append(round(a, 5))
+            dataTabela["bn"].append(round(b, 5))
+            dataTabela["ε"].append(round(epsilon,5))
+            dataTabela["xn"].append(round(xn, 5))
+            dataTabela["f(xn)"].append(round(f(xn), 5))
+        n+=1
         msg = f"A raiz aproximada é x1 = {xn}"
 
     print(msg)
-
 else:
     print("Não há raiz no intervalo pois: ")
     print(f"f(a) * f(b) = {f(a)*f(b)}")
@@ -62,7 +90,6 @@ else:
 
 linha()
 print("\n\n")
-
 
 
 
@@ -89,3 +116,29 @@ plt.legend()
 plt.grid(True)
  
 plt.savefig('grafico.png', dpi=300, bbox_inches='tight')
+
+
+
+
+#Gerando Tabela
+# Crie um DataFrame
+df = pd.DataFrame(dataTabela)
+
+# Crie a figura e o eixo
+fig, ax = plt.subplots(figsize=(5, 2))  # Tamanho da imagem (largura, altura)
+
+# Oculte o eixo
+ax.axis('off')
+
+# Desenhe a tabela no gráfico
+tabela = ax.table(cellText=df.values, colLabels=df.columns, cellLoc='center', loc='center')
+
+# Ajuste o tamanho da fonte
+tabela.auto_set_font_size(False)
+tabela.set_fontsize(12)
+
+# Ajuste o tamanho das células
+tabela.scale(2, 1.2)
+
+# Salve a tabela como uma imagem PNG
+plt.savefig('tabela.png', dpi=300, bbox_inches='tight')
